ab25d66501fe5779a0b0b07f6e246488da20b77a
BikramThangjam <tbsingh1211@gmail.com>
2024-04-09 13:31:19 +0530

    added register route and logic

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { register } from "./controllers/auth";
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -37,7 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage})
 
 // Routes
-app.post("/auth/register", upload.single("picture"), register)
+app.post("/auth/register", upload.single("picture"), register);
 
 // Mongoose setup
 const PORT = process.env.PORT || 5001;
