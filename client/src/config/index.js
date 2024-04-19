738e30f5f899b77a7e87f23417ca0a722e256fb7
BikramThangjam <tbsingh1211@gmail.com>
2024-04-19 21:46:51 +0530

    updated api url

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,7 +1,9 @@
 
 
 const config = {
-    API_URL: "http://localhost:3001",
+    // API_URL: "http://localhost:3001",
+    API_URL: "https://blendbuddies-socialmedia-api.vercel.app",
 }
 
 export const {API_URL} = config;
+
