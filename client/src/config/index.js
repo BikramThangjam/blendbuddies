46aa1b170cee87a4ca4c7011d80cea3043eedb6c
BikramThangjam <tbsingh1211@gmail.com>
2024-04-20 15:34:37 +0530

    api_url corrected

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -2,7 +2,7 @@
 
 const config = {
     // API_URL: "http://localhost:3001",
-    API_URL: "https://blendbuddies-server.vercel.app/",
+    API_URL: "https://blendbuddies-server.vercel.app",
 }
 
 export const {API_URL} = config;
